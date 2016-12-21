9631a7b4386b3f4f655b37584588dbacf03a53b1
dhanendras <dhanendra.chaudhary@gmail.com>
2016-12-21 21:54:18 +0530

    k

--- Chaincode/src/asset_code/asset.go
+++ Chaincode/src/asset_code/asset.go
@@ -439,8 +439,8 @@ if 		        d.Timestamp 	 == "TIMESTAMP" ||
 //=================================================================================================================================
 func (t *SimpleChaincode) distributor_to_dealer(stub  shim.ChaincodeStubInterface, d Diamond, caller string, caller_affiliation string, recipient_name string, recipient_affiliation string) ([]byte, error) {
 	
-	 console.log('[#] distributor_to_dealer',d.Status,d.Owner,caller_affiliation,recipient_affiliation);
-   
+	 
+   fmt.Printf("distributor_TO_DEALER2 %s %s %s %s ",d.Status,d.Owner,caller_affiliation,recipient_affiliation);
 	
 	if 		d.Status				== STATE_DISTRIBUTING	&& 
 			d.Owner					== caller				&& 
