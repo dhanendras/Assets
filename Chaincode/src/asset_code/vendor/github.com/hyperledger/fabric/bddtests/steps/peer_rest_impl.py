80bf26955bdb2574104251b382d1f777384309d6
dhanendras <dhanendra.chaudhary@gmail.com>
2016-12-25 13:54:55 +0530

    k

--- Chaincode/src/asset_code/vendor/github.com/hyperledger/fabric/bddtests/steps/peer_rest_impl.py
+++ Chaincode/src/asset_code/vendor/github.com/hyperledger/fabric/bddtests/steps/peer_rest_impl.py
@@ -16,36 +16,28 @@
 
 import requests
 from behave import *
-from peer_basic_impl import buildUrl
 from peer_basic_impl import getAttributeFromJSON
+from bdd_request_util import httpGetToContainerAlias
+from bdd_test_util import bdd_log
 
-import bdd_test_util
 
-
-@when(u'I request transaction certs with query parameters on "{containerName}"')
-def step_impl(context, containerName):
+@when(u'I request transaction certs with query parameters on "{containerAlias}"')
+def step_impl(context, containerAlias):
     assert 'table' in context, "table (of query parameters) not found in context"
     assert 'userName' in context, "userName not found in context"
     assert 'compose_containers' in context, "compose_containers not found in context"
 
-    ipAddress = bdd_test_util.ipFromContainerNamePart(containerName, context.compose_containers)
-    request_url = buildUrl(context, ipAddress, "/registrar/{0}/tcert".format(context.userName))
-    print("Requesting path = {0}".format(request_url))
     queryParams = {}
     for row in context.table.rows:
         key, value = row['key'], row['value']
         queryParams[key] = value
 
-    print("Query parameters = {0}".format(queryParams))
-    resp = requests.get(request_url, params=queryParams, headers={'Accept': 'application/json'}, verify=False)
-
-    assert resp.status_code == 200, "Failed to GET to %s:  %s" % (request_url, resp.text)
-    context.response = resp
-    print("")
+    endpoint = "/registrar/{0}/tcert".format(context.userName)
+    context.response = httpGetToContainerAlias(context, containerAlias, endpoint)
 
 @then(u'I should get a JSON response with "{expectedValue}" different transaction certs')
 def step_impl(context, expectedValue):
-    print(context.response.json())
-    foundValue = getAttributeFromJSON("OK", context.response.json(), "Attribute not found in response (OK)")
-    print(len(set(foundValue)))
+    bdd_log(context.response.json())
+    foundValue = getAttributeFromJSON("OK", context.response.json())
+    bdd_log(len(set(foundValue)))
     assert (len(set(foundValue)) == int(expectedValue)), "For attribute OK, expected different transaction cert of size (%s), instead found (%s)" % (expectedValue, len(set(foundValue)))
